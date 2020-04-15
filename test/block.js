3567671f0cee4adf45904c3f57807749bc663973
Attila <bitcoinfiles@protonmail.com>
2020-04-14 23:41:31 -0600

    Bump ver

--- test/block.js
+++ test/block.js
@@ -3,7 +3,7 @@ var expect = require('chai').expect;
 var index = require('../dist/index.js');
 
 const options = {
-    bitcoinfiles_api_base: 'http://localhost:8082',
+    // bitcoinfiles_api_base: 'http://localhost:8082',
 }
 
 describe('block', () => {
